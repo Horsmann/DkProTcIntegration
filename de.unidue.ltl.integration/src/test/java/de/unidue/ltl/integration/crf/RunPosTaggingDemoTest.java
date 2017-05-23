eb6e3f6cdf168be4fa5e8ab7faa1ea3a6675f748
Tobias Horsmann <tobias.horsmann@gmail.com>
2017-05-23 08:06:56 +0200

    fix of pos demo

--- de.unidue.ltl.integration/src/test/java/de/unidue/ltl/integration/crf/RunPosTaggingDemoTest.java
+++ de.unidue.ltl.integration/src/test/java/de/unidue/ltl/integration/crf/RunPosTaggingDemoTest.java
@@ -1,12 +1,13 @@
 package de.unidue.ltl.integration.crf;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import org.dkpro.tc.core.Constants;
 import org.dkpro.tc.evaluation.Id2Outcome;
 import org.dkpro.tc.evaluation.evaluator.EvaluatorBase;
 import org.dkpro.tc.evaluation.evaluator.EvaluatorFactory;
 import org.dkpro.tc.evaluation.measures.label.Accuracy;
+import org.dkpro.tc.ml.crfsuite.task.CRFSuiteTestTask;
 import org.junit.Test;
 
 import de.unidue.ltl.integration.ContextMemoryReport;
@@ -17,13 +18,13 @@ public class RunPosTaggingDemoTest
     public void run()
         throws Exception
     {
+        ContextMemoryReport.key = CRFSuiteTestTask.class.getName();
         new PosTagging().run();
-        ContextMemoryReport.key = PosTagging.class.getName();
 
         Id2Outcome o = new Id2Outcome(ContextMemoryReport.id2outcome, Constants.LM_SINGLE_LABEL);
         EvaluatorBase createEvaluator = EvaluatorFactory.createEvaluator(o, true, false);
         Double result = createEvaluator.calculateEvaluationMeasures()
                 .get(Accuracy.class.getSimpleName());
-        assertEquals(0.75, result, 0.0001);
+        assertTrue(result > 0.90);
     }
 }
