a44f681ef88aa8d85531eea1ff1e8cb09a7b6d51
Tobias Horsmann <tobias.horsmann@gmail.com>
2017-05-23 18:03:46 +0200

    liblinear regression demo added

---
+++ de.unidue.ltl.integration/src/main/java/de/unidue/ltl/integration/liblinearRegression/LiblinearRegressionDemo.java
@@ -0,0 +1,108 @@
+package de.unidue.ltl.integration.liblinearRegression;
+
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.collection.CollectionReaderDescription;
+import org.apache.uima.fit.factory.CollectionReaderFactory;
+import org.apache.uima.resource.ResourceInitializationException;
+import org.dkpro.lab.Lab;
+import org.dkpro.lab.task.Dimension;
+import org.dkpro.lab.task.ParameterSpace;
+import org.dkpro.tc.api.features.TcFeatureFactory;
+import org.dkpro.tc.api.features.TcFeatureSet;
+import org.dkpro.tc.core.Constants;
+import org.dkpro.tc.features.length.NrOfSentences;
+import org.dkpro.tc.features.length.NrOfTokens;
+import org.dkpro.tc.features.length.NrOfTokensPerSentence;
+import org.dkpro.tc.ml.ExperimentTrainTest;
+import org.dkpro.tc.ml.liblinear.LiblinearAdapter;
+import org.dkpro.tc.ml.liblinear.LiblinearTestTask;
+import org.dkpro.tc.ml.weka.WekaRegressionAdapter;
+
+import de.tudarmstadt.ukp.dkpro.core.tokit.BreakIteratorSegmenter;
+import de.unidue.ltl.integration.ContextMemoryReport;
+import de.unidue.ltl.integration.wekaRegression.EssayScoreReader;
+import weka.classifiers.functions.LinearRegression;
+
+public class LiblinearRegressionDemo
+    implements Constants
+{
+
+    public static void main(String[] args)
+        throws Exception
+    {
+        new LiblinearRegressionDemo().run();
+    }
+
+    public void run()
+        throws Exception
+    {
+        System.setProperty("DKPRO_HOME", "target/" + LiblinearRegressionDemo.class.getSimpleName());
+        ParameterSpace pSpace = getParameterSpace();
+
+        LiblinearRegressionDemo experiment = new LiblinearRegressionDemo();
+        experiment.runTrainTest(pSpace);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static ParameterSpace getParameterSpace()
+        throws ResourceInitializationException
+    {
+        // configure training and test data reader dimension
+        // train/test will use both, while cross-validation will only use the train part
+        // The reader is also responsible for setting the labels/outcome on all
+        // documents/instances it creates.
+        Map<String, Object> dimReaders = new HashMap<String, Object>();
+
+        CollectionReaderDescription readerTrain = CollectionReaderFactory.createReaderDescription(
+                EssayScoreReader.class, EssayScoreReader.PARAM_SOURCE_LOCATION,
+                "src/main/resources/essays/train.txt", EssayScoreReader.PARAM_LANGUAGE, "en");
+        dimReaders.put(DIM_READER_TRAIN, readerTrain);
+
+        CollectionReaderDescription readerTest = CollectionReaderFactory.createReaderDescription(
+                EssayScoreReader.class, EssayScoreReader.PARAM_SOURCE_LOCATION,
+                "src/main/resources/essays/test.txt", EssayScoreReader.PARAM_LANGUAGE, "en");
+        dimReaders.put(DIM_READER_TEST, readerTest);
+
+        Dimension<List<String>> dimClassificationArgs = Dimension.create(DIM_CLASSIFICATION_ARGS,
+                Arrays.asList(new String[] { "-s", "6" }));
+
+        Dimension<TcFeatureSet> dimFeatureSets = Dimension.create(DIM_FEATURE_SET,
+                new TcFeatureSet(TcFeatureFactory.create(NrOfTokens.class),
+                        TcFeatureFactory.create(NrOfSentences.class),
+                        TcFeatureFactory.create(NrOfTokensPerSentence.class)));
+
+        ParameterSpace pSpace = new ParameterSpace(Dimension.createBundle("readers", dimReaders),
+                Dimension.create(DIM_LEARNING_MODE, LM_REGRESSION),
+                Dimension.create(DIM_FEATURE_MODE, FM_DOCUMENT), dimFeatureSets,
+                dimClassificationArgs);
+
+        return pSpace;
+    }
+
+    // ##### TRAIN-TEST #####
+    protected void runTrainTest(ParameterSpace pSpace)
+        throws Exception
+    {
+        ExperimentTrainTest batch = new ExperimentTrainTest("LiblinearRegressionDemo",
+                LiblinearAdapter.class);
+        batch.setPreprocessing(getPreprocessing());
+        batch.setParameterSpace(pSpace);
+        batch.addReport(ContextMemoryReport.class);
+
+        // Run
+        Lab.getInstance().run(batch);
+    }
+
+    protected AnalysisEngineDescription getPreprocessing()
+        throws ResourceInitializationException
+    {
+        return createEngineDescription(createEngineDescription(BreakIteratorSegmenter.class));
+    }
+}
---
+++ de.unidue.ltl.integration/src/test/java/de/unidue/ltl/integration/liblinearRegression/RunLiblinearRegressionDemoTest.java
@@ -0,0 +1,30 @@
+package de.unidue.ltl.integration.liblinearRegression;
+
+import static org.junit.Assert.assertEquals;
+
+import org.dkpro.tc.core.Constants;
+import org.dkpro.tc.evaluation.Id2Outcome;
+import org.dkpro.tc.evaluation.evaluator.EvaluatorBase;
+import org.dkpro.tc.evaluation.evaluator.EvaluatorFactory;
+import org.dkpro.tc.evaluation.measures.regression.SpearmanCorrelation;
+import org.dkpro.tc.ml.weka.task.WekaTestTask;
+import org.junit.Test;
+
+import de.unidue.ltl.integration.ContextMemoryReport;
+
+public class RunLiblinearRegressionDemoTest
+{
+    @Test
+    public void run()
+        throws Exception
+    {
+        ContextMemoryReport.key = WekaTestTask.class.getName();
+        new LiblinearRegressionDemo().run();
+
+        Id2Outcome o = new Id2Outcome(ContextMemoryReport.id2outcome, Constants.LM_REGRESSION);
+        EvaluatorBase createEvaluator = EvaluatorFactory.createEvaluator(o, true, false);
+        Double result = createEvaluator.calculateEvaluationMeasures()
+                .get(SpearmanCorrelation.class.getSimpleName());
+        assertEquals(0.7731957, result, 0.01);
+    }
+}
