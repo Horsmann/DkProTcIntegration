e7ddb856f258c1792d937ea49e0b76eab532887f
Tobias Horsmann <tobias.horsmann@gmail.com>
2017-05-23 13:31:37 +0200

    weka document class

---
+++ de.unidue.ltl.integration/src/main/java/de/unidue/ltl/integration/weka/WekaTwentyNewsgroupsDemo.java
@@ -0,0 +1,145 @@
+/**
+ * Copyright 2017
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ */
+package de.unidue.ltl.integration.weka;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.collection.CollectionReaderDescription;
+import org.apache.uima.fit.factory.AnalysisEngineFactory;
+import org.apache.uima.fit.factory.CollectionReaderFactory;
+import org.apache.uima.resource.ResourceInitializationException;
+import org.dkpro.lab.Lab;
+import org.dkpro.lab.task.BatchTask.ExecutionPolicy;
+import org.dkpro.lab.task.Dimension;
+import org.dkpro.lab.task.ParameterSpace;
+import org.dkpro.tc.api.features.TcFeatureFactory;
+import org.dkpro.tc.api.features.TcFeatureSet;
+import org.dkpro.tc.core.Constants;
+import org.dkpro.tc.features.length.NrOfTokens;
+import org.dkpro.tc.features.length.NrOfTokensPerSentence;
+import org.dkpro.tc.features.ngram.LuceneNGram;
+import org.dkpro.tc.ml.ExperimentTrainTest;
+import org.dkpro.tc.ml.report.BatchRuntimeReport;
+import org.dkpro.tc.ml.report.BatchTrainTestReport;
+import org.dkpro.tc.ml.weka.WekaClassificationAdapter;
+
+import de.tudarmstadt.ukp.dkpro.core.tokit.BreakIteratorSegmenter;
+import de.unidue.ltl.integration.ContextMemoryReport;
+import de.unidue.ltl.integration.crf.PosTagging;
+import weka.classifiers.bayes.NaiveBayes;
+import weka.classifiers.functions.SMO;
+import weka.classifiers.functions.supportVector.PolyKernel;
+
+/**
+ * This a pure Java-based experiment setup of the TwentyNewsgroupsExperiment.
+ * 
+ * Defining the parameters directly in this class makes on-the-fly changes more difficult when the
+ * experiment is run on a server.
+ * 
+ * For these cases, the self-sufficient Groovy versions are more suitable, since their source code
+ * can be changed and then executed without pre-compilation.
+ */
+public class WekaTwentyNewsgroupsDemo
+    implements Constants
+{
+    public static final String LANGUAGE_CODE = "en";
+
+    public static final String corpusFilePathTrain = "src/main/resources/20newsgroup/train";
+    public static final String corpusFilePathTest = "src/main/resources/20newsgroup/test";
+
+    public static void main(String[] args)
+        throws Exception
+    {
+
+        System.setProperty("DKPRO_HOME", "target/" + PosTagging.class.getSimpleName());
+
+        ParameterSpace pSpace = getParameterSpace();
+
+        WekaTwentyNewsgroupsDemo experiment = new WekaTwentyNewsgroupsDemo();
+        // experiment.runCrossValidation(pSpace);
+        experiment.runTrainTest(pSpace);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static ParameterSpace getParameterSpace()
+        throws ResourceInitializationException
+    {
+        // configure training and test data reader dimension
+        // train/test will use both, while cross-validation will only use the train part
+        Map<String, Object> dimReaders = new HashMap<String, Object>();
+
+        CollectionReaderDescription readerTrain = CollectionReaderFactory.createReaderDescription(
+                TwentyNewsgroupsCorpusReader.class,
+                TwentyNewsgroupsCorpusReader.PARAM_SOURCE_LOCATION, corpusFilePathTrain,
+                TwentyNewsgroupsCorpusReader.PARAM_LANGUAGE, LANGUAGE_CODE,
+                TwentyNewsgroupsCorpusReader.PARAM_PATTERNS, "*.txt");
+        dimReaders.put(DIM_READER_TRAIN, readerTrain);
+
+        CollectionReaderDescription readerTest = CollectionReaderFactory.createReaderDescription(
+                TwentyNewsgroupsCorpusReader.class,
+                TwentyNewsgroupsCorpusReader.PARAM_SOURCE_LOCATION, corpusFilePathTest,
+                TwentyNewsgroupsCorpusReader.PARAM_LANGUAGE, LANGUAGE_CODE,
+                TwentyNewsgroupsCorpusReader.PARAM_PATTERNS, "*.txt");
+        dimReaders.put(DIM_READER_TEST, readerTest);
+
+        Dimension<List<String>> dimClassificationArgs = Dimension.create(DIM_CLASSIFICATION_ARGS,
+                Arrays.asList(new String[] { SMO.class.getName(), "-C", "1.0", "-K",
+                        PolyKernel.class.getName() + " " + "-C -1 -E 2" }));
+
+        Dimension<TcFeatureSet> dimFeatureSets = Dimension.create(DIM_FEATURE_SET, new TcFeatureSet(
+                TcFeatureFactory.create(NrOfTokensPerSentence.class),
+                TcFeatureFactory.create(NrOfTokens.class),
+                TcFeatureFactory.create(LuceneNGram.class, LuceneNGram.PARAM_NGRAM_USE_TOP_K, 1000,
+                        LuceneNGram.PARAM_NGRAM_MIN_N, 1, LuceneNGram.PARAM_NGRAM_MAX_N, 3)));
+
+        ParameterSpace pSpace = new ParameterSpace(Dimension.createBundle("readers", dimReaders),
+                Dimension.create(DIM_LEARNING_MODE, LM_SINGLE_LABEL),
+                Dimension.create(DIM_FEATURE_MODE, FM_DOCUMENT), dimFeatureSets,
+                dimClassificationArgs);
+
+        return pSpace;
+    }
+
+    protected void runTrainTest(ParameterSpace pSpace)
+        throws Exception
+    {
+
+        ExperimentTrainTest batch = new ExperimentTrainTest("TwentyNewsgroupsTrainTest",
+                WekaClassificationAdapter.class);
+        batch.setPreprocessing(getPreprocessing());
+        batch.setParameterSpace(pSpace);
+        batch.setExecutionPolicy(ExecutionPolicy.RUN_AGAIN);
+        batch.addReport(BatchTrainTestReport.class);
+        batch.addReport(ContextMemoryReport.class);
+
+        // Run
+        Lab.getInstance().run(batch);
+    }
+
+    protected AnalysisEngineDescription getPreprocessing()
+        throws ResourceInitializationException
+    {
+
+        return AnalysisEngineFactory.createEngineDescription(BreakIteratorSegmenter.class);
+    }
+}
