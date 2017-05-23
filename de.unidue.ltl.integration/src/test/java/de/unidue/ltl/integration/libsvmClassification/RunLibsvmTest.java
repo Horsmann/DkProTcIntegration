3c8b23fa579a7bab0ded257b02d14d42d7ce2573
Tobias Horsmann <tobias.horsmann@gmail.com>
2017-05-23 17:11:32 +0200

    liblinear and libsvm test cases

---
+++ de.unidue.ltl.integration/src/test/java/de/unidue/ltl/integration/libsvmClassification/RunLibsvmTest.java
@@ -0,0 +1,32 @@
+package de.unidue.ltl.integration.libsvmClassification;
+
+import static org.junit.Assert.assertEquals;
+
+import org.dkpro.tc.core.Constants;
+import org.dkpro.tc.evaluation.Id2Outcome;
+import org.dkpro.tc.evaluation.evaluator.EvaluatorBase;
+import org.dkpro.tc.evaluation.evaluator.EvaluatorFactory;
+import org.dkpro.tc.evaluation.measures.label.Accuracy;
+import org.dkpro.tc.ml.libsvm.LibsvmTestTask;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import de.unidue.ltl.integration.ContextMemoryReport;
+
+public class RunLibsvmTest
+{
+    @Ignore
+    @Test
+    public void run()
+        throws Exception
+    {
+        ContextMemoryReport.key = LibsvmTestTask.class.getName();
+        new LibsvmNewsgroupsDemo().run();
+
+        Id2Outcome o = new Id2Outcome(ContextMemoryReport.id2outcome, Constants.LM_SINGLE_LABEL);
+        EvaluatorBase createEvaluator = EvaluatorFactory.createEvaluator(o, true, false);
+        Double result = createEvaluator.calculateEvaluationMeasures()
+                .get(Accuracy.class.getSimpleName());
+        assertEquals(0.599622, result, 0.01);
+    }
+}
