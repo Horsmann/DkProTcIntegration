20c7c319cd03e5e0e1b2d212a6a74bc726199b11
Tobias Horsmann <tobias.horsmann@gmail.com>
2017-05-23 14:58:02 +0200

    libsvm regression

--- de.unidue.ltl.integration/src/main/java/de/unidue/ltl/integration/libsvmRegression/LibsvmRegressionDemo.java
+++ de.unidue.ltl.integration/src/main/java/de/unidue/ltl/integration/libsvmRegression/LibsvmRegressionDemo.java
@@ -1,4 +1,22 @@
-package de.unidue.ltl.integration.wekaRegression;
+/**
+ * Copyright 2017
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ */
+package de.unidue.ltl.integration.libsvmRegression;
 
 import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
 
@@ -12,6 +30,7 @@ import org.apache.uima.collection.CollectionReaderDescription;
 import org.apache.uima.fit.factory.CollectionReaderFactory;
 import org.apache.uima.resource.ResourceInitializationException;
 import org.dkpro.lab.Lab;
+import org.dkpro.lab.task.BatchTask.ExecutionPolicy;
 import org.dkpro.lab.task.Dimension;
 import org.dkpro.lab.task.ParameterSpace;
 import org.dkpro.tc.api.features.TcFeatureFactory;
@@ -20,43 +39,43 @@ import org.dkpro.tc.core.Constants;
 import org.dkpro.tc.features.length.NrOfSentences;
 import org.dkpro.tc.features.length.NrOfTokens;
 import org.dkpro.tc.features.length.NrOfTokensPerSentence;
-import org.dkpro.tc.features.ngram.LuceneNGram;
 import org.dkpro.tc.ml.ExperimentTrainTest;
-import org.dkpro.tc.ml.weka.WekaRegressionAdapter;
+import org.dkpro.tc.ml.libsvm.LibsvmAdapter;
+import org.dkpro.tc.ml.report.BatchTrainTestReport;
 
 import de.tudarmstadt.ukp.dkpro.core.tokit.BreakIteratorSegmenter;
 import de.unidue.ltl.integration.ContextMemoryReport;
-import weka.classifiers.functions.LinearRegression;
-import weka.classifiers.functions.Logistic;
+import de.unidue.ltl.integration.wekaRegression.EssayScoreReader;
 
-public class WekaRegressionDemo
+public class LibsvmRegressionDemo
     implements Constants
 {
+    public static final String LANGUAGE_CODE = "en";
+
+    public static final String corpusFilePathTrain = "src/main/resources/data/twentynewsgroups/bydate-train";
+    public static final String corpusFilePathTest = "src/main/resources/data/twentynewsgroups/bydate-test";
 
     public static void main(String[] args)
         throws Exception
     {
-        new WekaRegressionDemo().run();
+        new LibsvmRegressionDemo().run();
     }
 
     public void run()
         throws Exception
     {
-        System.setProperty("DKPRO_HOME", "target/" + WekaRegressionDemo.class.getSimpleName());
+        System.setProperty("DKPRO_HOME", "target/" + LibsvmRegressionDemo.class.getSimpleName());
         ParameterSpace pSpace = getParameterSpace();
 
-        WekaRegressionDemo experiment = new WekaRegressionDemo();
+        LibsvmRegressionDemo experiment = new LibsvmRegressionDemo();
         experiment.runTrainTest(pSpace);
     }
 
-    @SuppressWarnings("unchecked")
     public static ParameterSpace getParameterSpace()
         throws ResourceInitializationException
     {
         // configure training and test data reader dimension
         // train/test will use both, while cross-validation will only use the train part
-        // The reader is also responsible for setting the labels/outcome on all
-        // documents/instances it creates.
         Map<String, Object> dimReaders = new HashMap<String, Object>();
 
         CollectionReaderDescription readerTrain = CollectionReaderFactory.createReaderDescription(
@@ -69,13 +88,15 @@ public class WekaRegressionDemo
                 "src/main/resources/essays/test.txt", EssayScoreReader.PARAM_LANGUAGE, "en");
         dimReaders.put(DIM_READER_TEST, readerTest);
 
+        @SuppressWarnings("unchecked")
         Dimension<List<String>> dimClassificationArgs = Dimension.create(DIM_CLASSIFICATION_ARGS,
-                Arrays.asList(new String[] { LinearRegression.class.getName() }));
+                Arrays.asList(new String[] { "-s", LibsvmAdapter.PARAM_SVM_TYPE_NU_SVR_REGRESSION,
+                        "-c", "100" }));
 
         Dimension<TcFeatureSet> dimFeatureSets = Dimension.create(DIM_FEATURE_SET,
                 new TcFeatureSet(TcFeatureFactory.create(NrOfTokens.class),
                         TcFeatureFactory.create(NrOfSentences.class),
-                TcFeatureFactory.create(NrOfTokensPerSentence.class)));
+                        TcFeatureFactory.create(NrOfTokensPerSentence.class)));
 
         ParameterSpace pSpace = new ParameterSpace(Dimension.createBundle("readers", dimReaders),
                 Dimension.create(DIM_LEARNING_MODE, LM_REGRESSION),
@@ -89,10 +110,13 @@ public class WekaRegressionDemo
     protected void runTrainTest(ParameterSpace pSpace)
         throws Exception
     {
-        ExperimentTrainTest batch = new ExperimentTrainTest("WekaRegressionDemo",
-                WekaRegressionAdapter.class);
+
+        ExperimentTrainTest batch = new ExperimentTrainTest("LibsvmRegression",
+                LibsvmAdapter.class);
         batch.setPreprocessing(getPreprocessing());
         batch.setParameterSpace(pSpace);
+        batch.setExecutionPolicy(ExecutionPolicy.RUN_AGAIN);
+        batch.addReport(BatchTrainTestReport.class);
         batch.addReport(ContextMemoryReport.class);
 
         // Run
@@ -102,6 +126,6 @@ public class WekaRegressionDemo
     protected AnalysisEngineDescription getPreprocessing()
         throws ResourceInitializationException
     {
-        return createEngineDescription(createEngineDescription(BreakIteratorSegmenter.class));
+        return createEngineDescription(BreakIteratorSegmenter.class);
     }
 }
